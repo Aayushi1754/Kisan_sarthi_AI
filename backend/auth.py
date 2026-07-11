5500e71ee49318872175aa95cdcb3baf00c32c45
Aayushi Goyal <aayushi1754@gmail.com>
2026-07-11 16:17:43 +0530

    Implemented JWT authentication with login, register and protected routers

--- backend/auth.py
+++ backend/auth.py
@@ -35,3 +35,19 @@ def create_access_token(data: dict):
         SECRET_KEY,
         algorithm=ALGORITHM
     )
+from fastapi import HTTPException, Depends, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+security = HTTPBearer()
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,11 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from database import get_db
 import models
-from auth import hash_password
-from schemas import UserRegister, UserResponse
+from auth import (
+    hash_password,verify_password, create_access_token,
+    verify_token,
+)   
+from schemas import UserRegister, UserResponse,UserLogin, Token
 from schemas import FeatureCreate
 from schemas import FeatureCreate, FeatureResponse
 Base.metadata.create_all(bind=engine)
@@ -236,3 +239,36 @@ def register(user: UserRegister, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+@app.post("/api/auth/login", response_model=Token)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        data={"sub": db_user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+@app.get("/api/profile")
+def profile(user=Depends(verify_token)):
+    return {
+        "message": "Welcome!",
+        "user": user
+    }
